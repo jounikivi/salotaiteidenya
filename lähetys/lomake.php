395089d36924868ec5bc6460306761c2367e8b8d
jounikivi <38974220+jounikivi@users.noreply.github.com>
2019-07-16 11:12:36 +0300

    Add files via upload

--- lähetys/lomake.php
+++ lähetys/lomake.php
@@ -28,9 +28,9 @@ $x13=$_GET['tapahtumainfo'];
 $sql = "INSERT INTO tapahtumat (Järjestäjän_nimi, Tapahtuman_nimi, Kategoria, Aloitusaika, päättymisaika, Osoite, Yhteystiedot, Järjestäjännettisivut, Tapahtumankuvaus, tapahtumainfo) VALUES ('$x2','$x3','$x4','$x5','$x6','$x7','$x10','$x11','$x12','$x13');";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
+    echo "Tapahtuma lisätty";
 } else {
-    echo "Error updating record: " . $conn->error;
+    echo "Tapahtuman lisäys ei onnistunut: " . $conn->error;
 }
 
 $conn->close();
@@ -42,6 +42,16 @@ $conn->close();
 
 <script>
 function goBack() {
-  window.history.back();
+   window.history.back();
+ }
+</script>
+
+<button onclick="gohome()">Palaa etusivulle</button>
+
+<script>
+function gohome()
+{
+window.location.href="../index.html"
 }
 </script>
+
